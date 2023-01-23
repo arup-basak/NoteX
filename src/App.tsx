4bb55d4079db00846be0310357c0d38b8e0cfb5e
Arup Basak <email.arupbasak@gmail.com>
2023-01-23 22:05:13 +0530

    added Notecard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NoteCard from './components/NoteCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-w-[600px]'>
+      <NoteCard heading="This is Heading" time="f" note="This is Note" />
+      <NoteCard heading="This is Heading" time="f" note="This is Note"/>
+      <NoteCard heading="This is Heading" time="f" note="This is Note"/>
+      <NoteCard heading="This is Heading" time="f" note="This is Note"/>
     </div>
   );
 }
---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface BoxData {
+    heading: string,
+    time: string,
+    note: string
+}
+
+const NoteCard = (props: BoxData) => {
+  return (
+    <div className='grid grid-cols-2 select-none m-2 p-2 bg-red-400 rounded-md hover:scale-[1.01] cursor-pointer transition'>
+          <div className=''>{props.heading}</div>
+          <div className=''>{props.time}</div>
+          <div className='grid col-start-1 col-end-3'>{props.note}</div>
+    </div>
+  )
+}
+
+export default NoteCard
