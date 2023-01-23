a53daa61e3496caf052069612e1d7c2ec5175d19
Arup Basak <email.arupbasak@gmail.com>
2023-01-24 00:34:20 +0530

    App.tsx can fetch from local file "note-data.json"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import NoteCard from './components/NoteCard';
+import data from './locals/note-data.json'
 
-const data = [
-    {
-      "heading": "This is Heading",
-      "time": "19:38",
-      "note": "This is Note"
-    },
-    {
-      "heading": "This is Heading2",
-      "time": "19:48",
-      "note": "This is Note2"
-    },
-    {
-      "heading": "This is Heading3",
-      "time": "19:18",
-      "note": "This is Note3"
-    }
-  ]
 
 function App() {
   return (
     <div className='min-w-[600px]'>
-      {data.map(function (res, i) {
-        return <NoteCard heading={res.heading} time={res.time} note={res.note} id={i} />;
+      {data.map((res, i) => {
+        return (<NoteCard
+          id={i}
+          heading={res.heading}
+          time={res.time}
+          note={res.note} />);
       })}
     </div>
   );
