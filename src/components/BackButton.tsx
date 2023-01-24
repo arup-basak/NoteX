1cec5cdae55517e207ea088a5e0a8d313ebaa5a1
Arup Basak <email.arupbasak@gmail.com>
2023-01-24 11:49:10 +0530

    jsx -> tsx

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  goBack
+} from 'react-chrome-extension-router';
+
+const BackButton = () => {
+  return (
+    <button
+      type="button"
+      data-mdb-ripple="true"
+      data-mdb-ripple-color="light"
+      onClick={() => goBack()}
+      className="inline-block w-min p-2 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+      &#8249;
+    </button>
+  )
+}
+export default BackButton
--- src/pages/NoteEditor.tsx
+++ src/pages/NoteEditor.tsx
@@ -4,9 +4,9 @@ const NoteEditor = (id: number) => (
   <div className='flex flex-col'>
     <BackButton />
     <input type="text"
-      className='w-max p-2 m-2 outline-none bg-blue-400 w-100%' />
+      className='w-fit p-2 m-2 outline-none bg-blue-400 text-lg'/>
     <textarea
-      className='w-max p-2 m-2 bg-red-500 resize-none outline-none'></textarea>
+      className='w-full p-2 m-2 bg-red-500 resize-none outline-none text-xl'></textarea>
     {/* <button onClick={() => goTo(Three, { id })}>
       Click me to go to component Three!
     </button> */}
