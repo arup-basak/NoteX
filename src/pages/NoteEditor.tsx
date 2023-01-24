4377eb46fba6a0fbf70933bc2c2b51c91715f341
Arup Basak <email.arupbasak@gmail.com>
2023-01-24 12:10:32 +0530

    Added Time Function

--- src/pages/NoteEditor.tsx
+++ src/pages/NoteEditor.tsx
@@ -1,4 +1,5 @@
 import BackButton from "../components/BackButton";
+import PopUp from "./PopUp";
 
 const NoteEditor = (id: number) => (
   <div className='flex flex-col'>
@@ -6,7 +7,7 @@ const NoteEditor = (id: number) => (
     <input type="text"
       className='w-fit p-2 m-2 outline-none bg-blue-400 text-lg'/>
     <textarea
-      className='w-full p-2 m-2 bg-red-500 resize-none outline-none text-xl'></textarea>
+      className='w-full p-2 m-2 bg-red-500 resize-none outline-none text-xl h-[400px]'></textarea>
     {/* <button onClick={() => goTo(Three, { id })}>
       Click me to go to component Three!
     </button> */}
--- src/pages/PopUp.tsx
+++ src/pages/PopUp.tsx
@@ -12,7 +12,25 @@ import {
     getComponentStack,
 } from 'react-chrome-extension-router';
 
+const time = ():string => {
+    const date = new Date();
+    const min = date.getMinutes();
+    if (min < 10) {
+        return date.getHours() + ":0" + min;
+    }
+    return date.getHours() + ":" + min;
+}
+
+const update = (id: number, heading: string, note: string): void => {
+    data[id].heading = heading
+    data[id].note = note
+    data[id].time = time()
+}
+
 const PopUp = () => {
+    if (data.length == 0) {
+        return <div>No Data</div>
+    }
     return (
         <div>
             {data.map((res, i) => {
